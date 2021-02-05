a220bd4480b0aeb9c25d2b2b3fd44e1a7d48b7be
Sam Wu <swu@naitca>
2021-02-05 16:16:04 -0700

    fixed IntelliJ buggie code in persistence.xml

--- dmit2015-instructor-oracle-human-resources/src/main/java/dmit2015/hr/config/ApplicationConfig.java
+++ dmit2015-instructor-oracle-human-resources/src/main/java/dmit2015/hr/config/ApplicationConfig.java
@@ -4,11 +4,11 @@ import javax.annotation.sql.DataSourceDefinition;
 import javax.enterprise.context.ApplicationScoped;
 
 @DataSourceDefinition(
-        name = "java:app/datasources/oracleHrDs",
-        className = "oracle.jdbc.pool.OracleDataSource",
-        url = "jdbc:oracle:thin:@localhost:11521/XEPDB1",
-        user = "HR",
-        password = "Password2015"
+    name = "java:app/datasources/oracleHrDs",
+    className = "oracle.jdbc.pool.OracleDataSource",
+    url="jdbc:oracle:thin:@//localhost:11521/XEPDB1",
+    user = "HR",
+    password = "Password2015"
 )
 
 @ApplicationScoped  // This is an application-scoped CDI managed bean
